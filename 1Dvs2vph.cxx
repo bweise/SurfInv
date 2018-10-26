6b1a654f42eb2bd3b0a9863ac63c1a31b2a3b0c5
Bernhard Weise <bweise@geophysik.uni-muenchen.de>
2018-10-26 14:36:03 +0200

    included computation of R matrix

--- 1Dvs2vph.cxx
+++ 1Dvs2vph.cxx
@@ -22,7 +22,7 @@ std::vector<double> periods = {0.1};
 
 // Kreiswellenzahlen-Limits in rad/m
 std::vector<double> k_lim = {20*M_PI/1000,20*M_PI/200};
-int nk = 3; //Anzahl Wellenzahlen zum durchprobieren
+int nk = 5; //Anzahl Wellenzahlen zum durchprobieren
 
 // Definition 1D Modell
 std::vector<double> depth = {0,25};	// Tiefe Schichtgrenzen [m]
@@ -58,27 +58,35 @@ std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp> compute_T(double w, double k, double v
 	return std::make_tuple(T1212,T1213,T1214,T1224,T1234);
 }
 
-std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp> compute_G(dcomp hn, dcomp kn, double k, double dn, double w, double vp, double dens){
+std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp> compute_G(dcomp hn, dcomp kn, double k, double dn, double w, double vp, double dens){
 	
 	dcomp hnn = hn/k;
 	dcomp knn = kn/k;
 	
+	dcomp SH = 0;
+	dcomp CH = 0;
+	dcomp SK = 0;
+	dcomp CK = 0;
+	
+	
 	if (imag(hnn)==0){
-		dcomp SH = 0.5*(1.0-exp((-2.0)*dn*hn))/hnn;
-		dcomp CH = 0.5*(1.0+exp((-2.0)*dn*hn));
+		SH = 0.5*(1.0-exp((-2.0)*dn*hn))/hnn;
+		CH = 0.5*(1.0+exp((-2.0)*dn*hn));
 	}
 	else{
-		dcomp SH = sin((-1.0)*i*dn*hn)/hnn;
-		dcomp CH = cos((-1.0)*i*dn*hn);
+		SH = sin((-1.0)*i*dn*hn)/hnn;
+		CH = cos((-1.0)*i*dn*hn);
 	}
 	if (imag(knn)==0){
-		dcomp SK = 0.5*(1.0-exp((-2.0)*dn*kn))/knn;
-		dcomp CK = 0.5*(1.0+exp((-2.0)*dn*kn));
+		SK = 0.5*(1.0-exp((-2.0)*dn*kn))/knn;
+		CK = 0.5*(1.0+exp((-2.0)*dn*kn));
 	}
 	else{
-		dcomp SK = sin((-1.0)*i*dn*kn)/knn;
-		dcomp CK = cos((-1.0)*i*dn*kn);
+		SK = sin((-1.0)*i*dn*kn)/knn;
+		CK = cos((-1.0)*i*dn*kn);
 	}
+	
+	//cout << SH << "\t" << CH << "\t" << SK << "\t"<< CK << "\n";
 	
 	double gam = 2.0*pow(k,2)/pow((w/vp),2);
 	double a1 = pow(gam,2)-2.0*gam+1.0;
@@ -104,8 +112,19 @@ std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dcomp,dco
 	dcomp G2412 = c1*(a1*CH*SK-pow(gam,2)*pow(hnn,2)*SH*CK);
 	dcomp G2413 = pow(hnn,2)*SH*SK;
 	dcomp G3412 = pow(c1,2)*(2*pow(gam,2)*a1*CH*CK+(pow(a1,2)+pow(gam,2)*a5)*SH*SK);
-	
-	return std::make_tuple(G1212,G1213,iG1214,G1224,G1234,G1312,iG1314,G1324,iG1412,iG1413,G1423,G2412,G2413,G3412);
+	//cout << "G-Test: " << G1212 << " " << G1213 << " " << iG1214 << " " << G1224 << " " << G1234 << " " << G1312 << " " << iG1314 << " " << G1324 << " " << iG1412 << " " << iG1413 << " " << G1423 << " " << G2412 << " " << G2413 << " " << G3412 << " " << "\n";
+	return std::make_tuple(G1212,G1213,iG1214,G1224,G1234,G1312,iG1314,G1324,iG1412,iG1413,G1423,G2412,G2413,G3412,CH,CK,expcorr);
+}
+
+std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp> compute_R(dcomp G1212, dcomp G1213, dcomp iG1214, dcomp G1224, dcomp G1234, dcomp G1312, dcomp iG1314, dcomp G1324, dcomp iG1412, dcomp iG1413, dcomp G1423, dcomp G2412, dcomp G2413, dcomp G3412, dcomp T1212, dcomp T1213, dcomp T1214, dcomp T1224, dcomp T1234, double w, dcomp CH, dcomp CK, dcomp expcorr){
+	//cout << "G-Test: " << G1212 << " " << G1213 << " " << iG1214 << " " << G1224 << " " << G1234 << " " << G1312 << " " << iG1314 << " " << G1324 << " " << iG1412 << " " << iG1413 << " " << G1423 << " " << G2412 << " " << G2413 << " " << G3412 << " " << "\n";
+	//cout << "T-Test: " << T1212 << " " << T1213 << " " << T1214 << " " << T1224 << " " << T1234 << "\n";
+	dcomp R1212 = T1212*G1212+(T1213*G1312-2.0*T1214*iG1412+T1224*G2412-T1234*G3412)/pow(w,1);
+	dcomp R1213 = pow(w,2)*T1212*G1213+T1213*CH*CK-2.0*T1214*iG1413-T1224*G2413+T1234*G2412;
+	dcomp R1214 = pow(w,2)*T1212*iG1214+T1213*iG1314+T1214*(2.0*G1423+expcorr)-T1224*iG1413+T1234*iG1412;
+	dcomp R1224 = pow(w,2)*T1212*G1224+T1213*G1324-2.0*T1214*iG1314+T1224*CH*CK+T1234*G1312;
+	dcomp R1234 = (-1.0)*pow(w,2)*T1212*G1234+T1213*G1224-2.0*T1214*iG1214+T1224*G1213+T1234*G1212;
+	return std::make_tuple(R1212,R1213,R1214,R1224,R1234);
 }
 
 int main()
@@ -137,24 +156,31 @@ int main()
 			double k=k_lim[0]+kint*(k_lim[1]-k_lim[0])/nk;
 			cout << "Aktuelle Kreisfreq. & Wellenzahl: " << w[freq] << "\t" << k << "\n";
 			
+			std::tuple<dcomp,dcomp,dcomp,dcomp,dcomp> R;
+			
 			for(int n=nlay-1;n>=0;n--){
 				cout << "Schicht: " << n+1 << "\n";
 				cout << "Geschwindigkeiten: " << vp[n] << "\t" << vs[n] << "\n";
 				auto kvert = compute_kvert(w[freq], k, vp[n], vs[n]);
-				cout << "kvert: " << std::get<0>(kvert) << "\t" << std::get<1>(kvert) << "\n\n\n";
+				cout << "kvert: " << std::get<0>(kvert) << "\t" << std::get<1>(kvert) << "\n";
 				if (n==nlay-1){
-					auto T = compute_T(w[freq], k, vp[n], vs[n], mu, std::get<0>(kvert), std::get<1>(kvert));
-					cout << "T-Komponenten: " << std::get<0>(T) << "\t"
-						<< std::get<1>(T) << "\t"
-						<< std::get<2>(T) << "\t"
-						<< std::get<3>(T) << "\t"
-						<< std::get<4>(T) << "\n\n\n";
+					R = compute_T(w[freq], k, vp[n], vs[n], mu, std::get<0>(kvert), std::get<1>(kvert));
+					cout << "R-Komponenten: " << std::get<0>(R) << "\t"
+						<< std::get<1>(R) << "\t"
+						<< std::get<2>(R) << "\t"
+						<< std::get<3>(R) << "\t"
+						<< std::get<4>(R) << "\n\n\n";
 				}
-				else{
+				else {
 					double dn = depth[n+1]-depth[n];
 					cout << "Schichtdicke: " << dn << "m\n";
 					auto G = compute_G(std::get<0>(kvert), std::get<1>(kvert),k,dn,w[freq],vp[n],dens[n]);
-					cout << "Noch nicht programmiert!\n\n\n";
+					R = compute_R(std::get<0>(G), std::get<1>(G), std::get<2>(G), std::get<3>(G), std::get<4>(G), std::get<5>(G), std::get<6>(G), std::get<7>(G), std::get<8>(G), std::get<9>(G), std::get<10>(G), std::get<11>(G), std::get<12>(G), std::get<13>(G), std::get<0>(R), std::get<1>(R), std::get<2>(R), std::get<3>(R), std::get<4>(R), w[freq], std::get<14>(G), std::get<15>(G), std::get<16>(G));
+					cout << "R-Komponenten: " << std::get<0>(R) << "\t"
+						<< std::get<1>(R) << "\t"
+						<< std::get<2>(R) << "\t"
+						<< std::get<3>(R) << "\t"
+						<< std::get<4>(R) << "\n\n\n";
 				}
 			}
 			
